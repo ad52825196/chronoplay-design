42e462e9ceecb46a11d8d8ae44fe535feeecb3e7
Zhen Chen <me@zhen-chen.com>
2016-10-06 00:11:04 +1300

    Main menu and play mode

---
+++ Control.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Control : MonoBehaviour {
+
+	public void LoadScene(string name) {
+        SceneManager.LoadScene(name);
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
---
+++ CubeCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.name == "Cube") {
+            Destroy(col.gameObject);
+        }
+    }
+}
